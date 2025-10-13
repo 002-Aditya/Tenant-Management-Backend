4d6f2d44bc6f39b392bfe9b38002270c8f7061ec
Aditya Kumar <aditya.kumar@mpccloudconsulting.com>
2025-10-14 00:25:20 +0530

    otp function modified to send 6 characters OTP with 3 letters and 3 numbers

--- utils/common-utility-functions/generate-otp.js
+++ utils/common-utility-functions/generate-otp.js
@@ -1,5 +1,18 @@
 function generateOTP() {
-    return Math.floor(1000 + Math.random() * 9000).toString();
+    const getRandomLetter = () => String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    const getRandomDigit = () => Math.floor(Math.random() * 10).toString();
+
+    const letters = Array.from({ length: 3 }, getRandomLetter);
+    const digits = Array.from({ length: 3 }, getRandomDigit);
+
+    const otpArray = [...letters, ...digits];
+
+    for (let i = otpArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [otpArray[i], otpArray[j]] = [otpArray[j], otpArray[i]];
+    }
+
+    return otpArray.join('');
 }
 
 module.exports = generateOTP;
